559f8e873ee8daf1dcf8648f971d474724ca4b6d
Leslie Shea <leslieyshea@gmail.com>
2019-01-17 20:58:36 -0800

    declared variables for all inputs

---
+++ main.js
@@ -0,0 +1,18 @@
+function displayMadLib () {
+    // get inputs by id
+    var nounOneInput = document.getElementById('noun-1');
+    var pluralNounOneInput = document.getElementById('plural-noun-1');
+    var properNounOneInput = document.getElementById('proper-noun-1');
+    var adjectiveOneInput = document.getElementById('adjective-1');
+    var nounTwoInput = document.getElementById('noun-2');
+    var nounThreeInput = document.getElementById('noun-3');
+    var nounFourInput = document.getElementById('noun-4');
+
+    // get value of inputs
+
+
+    // make the mad lib
+
+
+    // display mad lib
+}
